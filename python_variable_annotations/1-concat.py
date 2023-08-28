16b8ff585662e7fcf8f5b17d800ce15fb2d7a49c
Air-KS <kevinrogeret@gmail.com>
2023-08-28 08:06:47 +0000

    Task 1: Concatenated str1 and str2

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env pyhton3
+'''
+    Function add that takes a float a and a float b
+    as arguments and returns their sum as a float.
+'''
+
+
+def str(str1: str, str2: str) -> str:
+    '''Return the concat of str1 and str2'''
+    return str1 + str2
