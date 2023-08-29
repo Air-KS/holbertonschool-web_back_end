db4ed79ec9cac1ac0a53cfeff1ddf4f11ebc9958
Air-KS <kevinrogeret@gmail.com>
2023-08-29 09:08:57 +0000

    add import AsyncGenerator from typing

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -5,9 +5,10 @@
 '''
 import asyncio
 import random
+from typing import AsyncGenerator
 
 
-async def async_generator():
+async def async_generator() -> AsyncGenerator[float, None, None]:
     ''' coroutine called async_generator that takes no arguments '''
     for _ in range(10):
         await asyncio.sleep(1)
