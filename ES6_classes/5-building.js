e60c79f6fd9a39f3e47df131ebfa972673db3a9b
Air-KS <kevinrogeret@gmail.com>
2023-09-13 15:44:27 +0000

    task 5

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,12 +1,12 @@
 export default class Building {
-  // Gère les erreurs et initialise les attributs de la classe
   constructor(sqft) {
-    if (this.constructor !== Building) {
+    this._sqft = sqft;
+  }
+
+  evacuationWarningMessage() {
+    if (this.constructor === Building || !this.evacuationWarningMessage) {
       throw new Error('Class extending Building must override evacuationWarningMessage');
     }
-
-    // Les attributs de classe
-    this._sqft = sqft;
   }
 
   // Getters et Setters pour les attributs
