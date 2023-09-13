59a16746fb994c28a5a32d04cb4baed028be9700
Air-KS <kevinrogeret@gmail.com>
2023-09-13 10:48:52 +0000

    Task 1

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -1,7 +1,8 @@
-import ClassRoom from "./0-classroom";
+import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
   return [
+    // return an array of 3 ClassRoom objects with the sizes 19, 20, and 34
     new ClassRoom(19),
     new ClassRoom(20),
     new ClassRoom(34),
