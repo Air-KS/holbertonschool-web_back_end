d4fd837d406a4fd1fcc22b3eedbc15aaed62c446
Air-KS <kevinrogeret@gmail.com>
2023-09-13 08:44:07 +0000

    Task 10

---
+++ ES6_basic/10-loops.js
@@ -0,0 +1,9 @@
+export default function appendToEachArrayValue(array, appendString) {
+  const newArray = [];
+
+  for (const value in array) {
+    newArray.push(appendString + value);
+  }
+
+  return newArray;
+}
