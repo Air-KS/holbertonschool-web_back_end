e80fa9743f48502a6e8b66b45c3e9f24d525ef6f
Air-KS <kevinrogeret@gmail.com>
2023-10-02 13:36:32 +0000

    Task 0 - Add comment

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,8 +1,8 @@
-// comment
+// Exporte une fonction qui simule une réponse API
 export default function getResponseFromAPI() {
-  return new Promise ((resolve) => {
-    setTimeout(() => {
-      resolve("Réponse de l'API");
-    }, 1000);
+  // Retourne une promesse
+  return new Promise((resolve) => {
+    // Résout la promesse après 1 seconde
+    setTimeout(() => resolve("Réponse de l'API"), 1000);
   });
 }
