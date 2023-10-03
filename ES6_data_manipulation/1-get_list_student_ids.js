4a76e263903c0fc9676a189ddc83d651481887b7
Air-KS <kevinrogeret@gmail.com>
2023-10-03 10:58:35 +0000

    Task 1

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,7 +1,7 @@
 // Function to extract student IDs from a list of student objects
 export default function getListStudentIds(list) {
   // Check if the provided argument is an array
-  if(!Array.isArray(list)) {
+  if (!Array.isArray(list)) {
     return [];
   }
   // Return an array of student IDs using the map function
