6e7b1111f2cf394e74f0f38bc75b8b8482f5af21
Air-KS <kevinrogeret@gmail.com>
2023-10-03 10:52:04 +0000

    Task 0

---
+++ ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,11 @@
+// Function to return a list of students with their details
+export default function getListStudents() {
+  return [
+    // Student details: Guillaume
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    // Student details: James
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    // Student details: Serena
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
+}
