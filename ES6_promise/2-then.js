68abd5668954143d99ebe247773cac522682444d
Air-KS <kevinrogeret@gmail.com>
2023-10-03 08:30:11 +0000

    Task 2

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -3,19 +3,9 @@ export default function handleResponseFromAPI(promise) {
   // Accept a promise as an argument
   return promise
     // If the promise resolves successfully
-    .then(() => ({
-      // Return a success response object
-      status: 200,
-      body: 'success',
-    }))
+    .then(() => ({ status: 200, body: 'success', }))
     // If the promise is rejected
-    .catch(() =>
-      // Return a new Error object
-      Error()
-    )
+    .catch(() => Error())
     // After either resolving or rejecting the promise
-    .finally(() =>
-      // Log a message indicating that a response was received from the API
-      console.log('Got a response from the API')
-    );
+    .finally(() => console.log('Got a response from the API'));
 }
