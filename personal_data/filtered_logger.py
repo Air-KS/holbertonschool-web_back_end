4bee71168a4926451e426a219c642c2dc2683c8e
Air-KS <kevinrogeret@gmail.com>
2024-02-19 10:40:21 +0100

    function called filter_datum that returns the log message obfuscated

---
+++ personal_data/filtered_logger.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Module to define the filter_datum function.
+"""
+
+import re
+from typing import List
+
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
+    """
+    Replace specified fields in the log message with the redaction string.
+
+    Arguments:
+        fields: List of strings representing fields to redact.
+        redaction: String to use for redacting the fields.
+        message: Log message containing the fields to be redacted.
+        separator: String representing the separator between fields in the message.
+
+    Returns:
+        The log message with specified fields redacted.
+    """
+    pattern = fr'({"|".join(fields)})=[^{separator}]+'
+    return re.sub(pattern, fr'\\1={redaction}', message)
