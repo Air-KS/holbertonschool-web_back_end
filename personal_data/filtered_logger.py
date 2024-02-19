fb512dde2bea1db23bdd5793a5938f62df86c2ca
Air-KS <kevinrogeret@gmail.com>
2024-02-19 10:47:36 +0100

    add comment for pycodestyle

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,24 +1,25 @@
 #!/usr/bin/env python3
 """
-Module to define the filter_datum function.
+0. Regex-ing
 """
 
 import re
 from typing import List
+
 
 def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str) -> str:
     """
     Replace specified fields in the log message with the redaction string.
 
-    Arguments:
-        fields: List of strings representing fields to redact.
-        redaction: String to use for redacting the fields.
-        message: Log message containing the fields to be redacted.
-        separator: String representing the separator between fields in the message.
+    fields: List of strings representing fields to redact.
+    redaction: String to use for redacting the fields.
+    message: Log message containing the fields to be redacted.
+    separator: String representing the separator between fields in the message.
 
     Returns:
         The log message with specified fields redacted.
     """
-    pattern = fr'({"|".join(fields)})=[^{separator}]+'
-    return re.sub(pattern, fr'\\1={redaction}', message)
+    return re.sub(fr'({"|".join(fields)})=[^{separator}]+',
+                  fr'\1={redaction}',
+                  message)
