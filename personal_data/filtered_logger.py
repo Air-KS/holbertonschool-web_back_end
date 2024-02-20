d201a040ce89675910c00208f2b558975554f119
Air-KS <kevinrogeret@gmail.com>
2024-02-20 12:28:24 +0100

    filtered

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -11,9 +11,9 @@ import os
 import mysql.connector
 from mysql.connector.connection import MySQLConnection
 
-PII_FIELDS: Tuple[str, str, str, str, str] = ("name", "email", "phone_number",
-                                              "address",
-                                              "social_security_number")
+PII_FIELDS: Tuple[str, ...] = ("name", "email", "phone_number", "address",
+                               "social_security_number",
+                               "password", "phone", "ssh")
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
