fd555035fba9e94a325b3013b31c2698e433794e
Air-KS <kevinrogeret@gmail.com>
2024-02-24 12:01:24 +0100

    app

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -13,30 +13,26 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
-AUTH_TYPE = getenv('AUTH_TYPE')
-
-if AUTH_TYPE == "auth":
-    from api.v1.auth.auth import Auth
-    auth = Auth()
-elif AUTH_TYPE == "basic_auth":
+AUTH_TYPE = os.getenv("AUTH_TYPE")
+if AUTH_TYPE == "basic_auth":
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
+elif AUTH_TYPE == "auth":
+    from api.v1.auth.auth import Auth
+    auth = Auth()
+
 
 @app.before_request
-def before_request() -> str:
-    """Execute before each request
-
-        Return:
-            String or nothing
-    """
+def handle_before_request():
+    """ Request validation """
     if auth is None:
         return
+    excluded_paths = [
+        '/api/v1/status/',
+        '/api/v1/unauthorized/',
+        '/api/v1/forbidden/']
 
-    expath = ['/api/v1/status/',
-              '/api/v1/unauthorized/',
-              '/api/v1/forbidden/']
-
-    if not (auth.require_auth(request.path, expath)):
+    if not (auth.require_auth(request.path, excluded_paths)):
         return
 
     if (auth.authorization_header(request)) is None:
@@ -44,6 +40,7 @@ def before_request() -> str:
 
     if (auth.current_user(request)) is None:
         abort(403)
+
 
 @app.errorhandler(404)
 def not_found(error) -> str:
@@ -54,26 +51,16 @@ def not_found(error) -> str:
 
 @app.errorhandler(401)
 def unauthorized(error) -> str:
-    """Handle a unauthorized access
-
-        Args:
-            error: Error catch
-
-        Return:
-            Info of the error
+    """
+    Unauthorized handler
     """
     return jsonify({"error": "Unauthorized"}), 401
 
 
 @app.errorhandler(403)
 def forbidden(error) -> str:
-    """Handle a forbidden resource
-
-        Args:
-            error: Error catch
-
-        Return:
-            Info of the error
+    """
+    Forbidden handler
     """
     return jsonify({"error": "Forbidden"}), 403
 
