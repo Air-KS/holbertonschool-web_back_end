062f3db5484c249a6fa6be5e19146c8a2c2b7123
Air-KS <kevinrogeret@gmail.com>
2024-02-26 12:58:55 +0100

    Test for cheaker

--- user_authentication_service/user.py
+++ user_authentication_service/user.py
@@ -1,7 +1,5 @@
-#!/usr/bin/pyhton3
-"""
-The model will have the following attributes:
-"""
+#!/usr/bin/env python3
+"""Class User for ORM"""
 
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
@@ -9,10 +7,10 @@ Base = declarative_base()
 
 
 class User(Base):
-    """ Class user """
+    """Representation of a user """
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
-    session_id = Column(String(250), nullable=True)
-    reset_token = Column(String(250), nullable=True)
+    session_id = Column(String(250))
+    reset_token = Column(String(250))
