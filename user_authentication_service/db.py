9b6d66172ca6551a2c2999003cab15227d0b4be3
Air-KS <kevinrogeret@gmail.com>
2024-02-26 15:45:17 +0100

    Test for cheaker

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -2,12 +2,11 @@
 """DB module
 """
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.orm.exc import NoResultFound
 
-from user import Base
-from user import User
+from user import Base, User
 
 
 class DB:
@@ -17,7 +16,7 @@ class DB:
     def __init__(self) -> None:
         """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
@@ -32,8 +31,27 @@ class DB:
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """ Add a new user to the database """
+        """
+            Add a new user to the database.
+        """
+        # Create a new User instance
         user = User(email=email, hashed_password=hashed_password)
+        # Add the User instance to the current SQLAlchemy session
         self._session.add(user)
+        # Commit the changes to the database to persist the new user
         self._session.commit()
+        # Return the User instance, now with an ID assigned by the db
         return user
+
+    def find_user_by(self, **kwargs) -> User:
+        """Find a user in the database by specified criteria."""
+        try:
+            # Effectue 1requ ds db pr récup 1er user = critères kwargs
+            user = self._session.query(User).filter_by(**kwargs).first()
+            if user is None:
+                raise NoResultFound(
+                    "Not found")
+            return user
+        except NoResultFound as e:
+            # e pr stock l instance de l except capturé ici
+            raise NoResultFound(f"Invalid: {e}")
