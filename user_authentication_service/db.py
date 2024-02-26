382e028b9401a037d0beb7f1696cd920b7608cf6
Air-KS <kevinrogeret@gmail.com>
2024-02-26 13:28:45 +0100

    class provided below to implement the add_user method

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -32,7 +32,7 @@ class DB:
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """ Comment """
+        """ Add a new user to the database """
         user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
         self._session.commit()
