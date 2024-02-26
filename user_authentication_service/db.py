fda650a331c0c64124ee54917bbaf544a0efe635
Air-KS <kevinrogeret@gmail.com>
2024-02-26 15:48:00 +0100

    db

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -31,27 +31,19 @@ class DB:
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """
-            Add a new user to the database.
-        """
-        # Create a new User instance
+        """ Add a new user to the database. """
         user = User(email=email, hashed_password=hashed_password)
-        # Add the User instance to the current SQLAlchemy session
         self._session.add(user)
-        # Commit the changes to the database to persist the new user
         self._session.commit()
-        # Return the User instance, now with an ID assigned by the db
         return user
 
     def find_user_by(self, **kwargs) -> User:
-        """Find a user in the database by specified criteria."""
+        """ Find a user in the database """
         try:
-            # Effectue 1requ ds db pr récup 1er user = critères kwargs
             user = self._session.query(User).filter_by(**kwargs).first()
             if user is None:
                 raise NoResultFound(
                     "Not found")
             return user
         except NoResultFound as e:
-            # e pr stock l instance de l except capturé ici
             raise NoResultFound(f"Invalid: {e}")
