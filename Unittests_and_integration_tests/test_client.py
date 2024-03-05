74c209a1fbc0d8191790eb1433629bd3a6126980
Air-KS <kevinrogeret@gmail.com>
2024-03-05 13:38:32 +0100

    [Add] - import GithubOrgClient

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -1,26 +1,27 @@
 #!/usr/bin python3
 """ Test client """
 
+from client import GithubOrgClient
 import unittest
 from parameterized import parameterized
 from unittest.mock import patch
 
 
 class TestGithubOrgClient(unittest.TestCase):
-    """ Class Github OGN Client """
+    """ Class Gitghub ORG Client """
 
     @parameterized.expand([
         ('google'),
         ('abc')
-        ])
+    ])
     @patch('client.get_json')
     def test_org(self, input, mock):
         """ Test the organization
 
-        args:
-            input: name oh the test_org
-            mock: Mock
+            args:
+                input: Name of the org
+                mock: Mock
         """
-        test_class = TestGithubOrgClient(input)
+        test_class = GithubOrgClient(input)
         test_class.org()
         mock.assert_called_once_with(f'https://api.github.com/orgs/{input}')
