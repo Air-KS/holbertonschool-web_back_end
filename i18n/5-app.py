a1810e623045aad31a25c1fc804710367db52cb7
Air-KS <kevinrogeret@gmail.com>
2024-03-08 19:15:00 +0100

    5

--- i18n/5-app.py
+++ i18n/5-app.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
 """ Basic Babel setup """
-from flask import Flask, render_template, request, g
-from flask_babel import Babel, _
-from typing import Union
+import flask
+from flask import Flask, render_template, g, request
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+babel = Babel(app)
 
 
 users = {
@@ -15,65 +19,51 @@ users = {
 
 class Config(object):
     """ Configuration Babel """
-    LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_TIMEZONE = 'UTC'
+    LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
-app = Flask(__name__, template_folder='templates')
 app.config.from_object(Config)
-babel = Babel(app)
 
 
-@app.before_request
-def before_request(login_as: int = None):
-    """ Request of each function
+@app.route("/", methods=['GET'])
+def index():
     """
-    user: dict = get_user()
-    print(user)
-    g.user = user
-
-
-def get_user() -> Union[dict, None]:
-    """ Get the user of the dict
-
-        Return User
-    """
-    login_user = request.args.get('login_as', None)
-
-    if login_user is None:
-        return None
-
-    user: dict = {}
-    user[login_user] = users.get(int(login_user))
-
-    return user[login_user]
-
-
-@babel.localeselector
-def get_locale():
-    """ Locale language
-
-        Return:
-            Best match to the language
-    """
-    locale = request.args.get('locale', None)
-
-    if locale and locale in app.config['LANGUAGES']:
-        return locale
-
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
-
-
-@app.route('/', methods=['GET'], strict_slashes=False)
-def hello_world():
-    """ Greeting
-
-        Return:
-            Initial template html
+    Rendre le modèle avec les informations utilisateur appropriées
     """
     return render_template('5-index.html')
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+def get_user() -> dict:
+    """Retrieve user based on login_as parameter"""
+    user_id = request.args.get('login_as')
+    try:
+        user_id = int(user_id)
+        if user_id in users:
+            return users[user_id]
+    except (ValueError, TypeError):
+        pass
+    return None
+
+
+@app.before_request
+def before_request():
+    """
+    Définir l'utilisateur global s'il est connecté
+    """
+    user = get_user()
+    if user:
+        g.user = user
+
+
+@babel.localeselector
+def get_locale():
+    """
+    Déterminer la langue préférée de l'utilisateur
+    """
+    requested_locale = request.args.get('locale')
+    if requested_locale in Config.LANGUAGES:
+        return requested_locale
+
+    return request.accept_languages.best_match(Config.LANGUAGES)
