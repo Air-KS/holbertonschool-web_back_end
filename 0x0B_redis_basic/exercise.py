731752a811c6088d49bd680884559536e5df7bcf
Air-KS <kevinrogeret@gmail.com>
2024-03-19 12:35:41 +0100

    [Pycodestyle]

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -6,11 +6,22 @@ from typing import Union
 
 
 class Cache:
+    """Cache class for storing data in Redis."""
+
     def __init__(self):
+        """Initialize Redis connection and flush the database."""
         self._redis = redis.Redis()
         self._redis.flushdb()
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
+        """Store data in Redis using a random key.
+
+        Args:
+            data: The data to be stored, can be str, bytes, int, or float.
+
+        Returns:
+            A string representing the randomly generated key.
+        """
         key = str(uuid.uuid4())
         self._redis.set(name=key, value=data)
         return key
