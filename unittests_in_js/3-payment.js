e2b504d2f11c3e1f13795a65da0ff5b17d39f1f0
Air-KS <kevinrogeret@gmail.com>
2024-03-26 11:09:27 +0100

    corrige red cheak 4-5

--- unittests_in_js/3-payment.js
+++ unittests_in_js/3-payment.js
@@ -1,8 +1,8 @@
 const Utils = require('./utils');
 
 function sendPaymentRequestToApi(totalAmount, totalShipping) {
-  const total = Utils.calculateNumber('SUM', totalAmount, totalShipping);
-  console.log(`The total is: ${total}`);
+  const sun = Utils.calculateNumber('SUM', totalAmount, totalShipping);
+  console.log(`The total is: ${sum}`);
 }
 
 module.exports = sendPaymentRequestToApi;
--- unittests_in_js/3-payment.test.js
+++ unittests_in_js/3-payment.test.js
@@ -1,29 +1,21 @@
-const sinon = require('sinon');
+// 3-payment.test.js
+
 const assert = require('assert');
+const sinon = require('sinon');
+const Utils = require('./utils.js');
+const sendPaymentRequestToApi = require('./3-payment.js');
 
-const Utils = require('./utils');
-const sendPaymentRequestToApi = require('./3-payment');
+describe('sendPaymentRequestToApi', function () {
+    it('should call Utils.calculateNumber with correct arguments', function () {
+        const spy = sinon.spy(Utils, 'calculateNumber');
+        const totalAmount = 100;
+        const totalShipping = 20;
 
-describe('sendPaymentRequestToApi', function() {
-  let calculateNumberSpy;
+        sendPaymentRequestToApi(totalAmount, totalShipping);
 
-  beforeEach(function() {
-    calculateNumberSpy = sinon.spy(Utils, 'calculateNumber');
-  });
+        sinon.assert.calledOnce(spy);
+        sinon.assert.calledWithExactly(spy, 'SUM', totalAmount, totalShipping);
 
-  afterEach(function() {
-    calculateNumberSpy.restore();
-  });
-
-  it('should call Utils.calculateNumber with correct arguments', function() {
-    sendPaymentRequestToApi(100, 20);
-    sinon.assert.calledWithExactly(calculateNumberSpy, 'SUM', 100, 20);
-  });
-
-  it('should log the correct total', function() {
-    const consoleSpy = sinon.spy(console, 'log');
-    sendPaymentRequestToApi(100, 20);
-    sinon.assert.calledWithExactly(consoleSpy, 'The total is: 120');
-    consoleSpy.restore();
-  });
+        spy.restore(); // Restore the original method after the test
+    });
 });
